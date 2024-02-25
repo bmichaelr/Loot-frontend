0ed3c505bdf2c1a9deae951f69050a6c51cc8fe4
bmichaelr <142949912+bmichaelr@users.noreply.github.com>
2024-02-25 11:36:37 -0800

    initial commit

---
+++ Loot/LootApp.swift
@@ -0,0 +1,17 @@
+//
+//  LootApp.swift
+//  Loot
+//
+//  Created by Benjamin Michael on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct LootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
