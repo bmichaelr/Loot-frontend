06e8aa9e4ed1cfe8911c466852f42e51d05c96e8
Kenna Chase <31677932+Kenna-Chase@users.noreply.github.com>
2024-02-28 12:48:08 -0800

    Created basic GameLobby and HomeMenu View Screens

---
+++ Loot/HomeMenuView.swift
@@ -0,0 +1,84 @@
+//
+//  HomeMenuView.swift
+//  Loot
+//
+//  Created by Kenna Chase on 2/26/24.
+//
+
+import SwiftUI
+
+struct HomeMenuView: View {
+    @State private var gameRoomKey: String = ""
+
+    var body: some View {
+        Spacer()
+        // Display the game title.
+        Text("Loot!")
+            .font(.largeTitle)
+        
+        VStack {
+            Spacer()
+            
+            Button {
+                //Create Game Logic
+                gameRoomKey = createGame()
+            } label: {
+                Text("Create Game")
+                    .foregroundColor(.black)
+                    .font(.title2)
+                    .bold()
+            }
+            .background(
+                Capsule(style: .continuous)
+                .fill(.gray)
+                .frame(width: 250, height: 50)
+            )
+            
+            Spacer()
+            
+            VStack {
+                TextField(
+                    "Enter Game Room Key ",
+                    text: $gameRoomKey
+                ).bold()
+                    .padding(.vertical, 30)
+                    .multilineTextAlignment(.center)
+                    .disableAutocorrection(true).onSubmit {
+                        print("Authenticating…")
+                    }
+               
+                    
+                Button("Join Game") {
+                    //Join Game Logic
+                    joinGame(gameKey: gameRoomKey)
+            
+                } .foregroundColor(.black)
+                    .font(.title2)
+                    .bold()
+                    .background(
+                        Capsule(style: .continuous)
+                        .fill(.gray)
+                        .frame(width: 250, height: 50)
+                    )
+            }
+
+          Spacer()
+            
+        }
+    }
+    
+    func joinGame(gameKey: String) {
+        print("\nAuthenticating ... ")
+        //Validate Game Logic
+        print("Joining game ...." + gameKey)
+    }
+    
+    func createGame() -> String {
+        print("\nCreating game .....")
+        return "FAKE_GAME_CODE"
+    }
+}
+
+#Preview {
+    HomeMenuView()
+}
