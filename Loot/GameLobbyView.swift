8f1f802e2d9a57dbf769948852a188c208884811
Kenna Chase <31677932+Kenna-Chase@users.noreply.github.com>
2024-03-11 14:46:27 -0700

    Added structure for listing players and barebones ready button

--- Loot/GameLobbyView.swift
+++ Loot/GameLobbyView.swift
@@ -8,37 +8,77 @@ import SwiftUI
 struct GameLobbyView: View {
     @ObservedObject var displayViewController = DisplayViewController.sharedViewDisplayController
 
+    let peopleTest: [String] = ["Name1", "Name2", "Name3", "Name4"]
+
     var body: some View {
+        VStack {
+
+        Spacer()
 
         HStack {
             Spacer()
 
             Text("Game Room Key: XXX-XXX")
-                    .font(.largeTitle)
+                    .font(.title)
                     .foregroundColor(.black)
                     .bold()
 
             Spacer()
        }
+        // Spacer()
 
-        VStack {
-            Button {
-                displayViewController.changeView(view: .gameView)
-            } label: {
-                Text("Temp Button: Start Game")
-                    .foregroundColor(.black)
-                    .font(.title2)
-                    .bold()
-            }
-            .background(
-                Capsule(style: .continuous)
-                .fill(.gray)
-                .frame(width: 250, height: 50)
-            )
+            Section {
+                            VStack(alignment: .leading, spacing: 0) {
+                                // ForEach(titles[key]!, id: \.self) { title in
+                                ForEach(peopleTest, id: \.self) { player in
 
-        }.padding()
+                                    HStack {
 
-        Spacer()
+                                        Image(systemName: "circle.fill")
+                                            .resizable()
+                                            .frame(width: 32.0, height: 32.0)
+                                            .padding()
+
+                                        Spacer()
+                                        Text(player)
+                                        Spacer()
+                                        Image(systemName: "xmark")
+                                            .foregroundColor(Color.red)
+                                        // Setup symbol change "checkmark"
+                                        Spacer()
+                                    }.overlay(
+                                        RoundedRectangle(cornerRadius: 10, style: .circular)
+                                            .stroke(Color.black, lineWidth: 1)
+                                            .frame(width: 350, height: 50)
+                                            // .border(Color.black, width: 3)
+                                    )// HSTACK
+                                    .padding(20)
+                                }//: LOOP
+
+                                Spacer()
+                                HStack {
+                                    Spacer()
+                                    Button {
+                                        // Ready Up Button Logic
+                                    } label: {
+                                        Text("Ready")
+                                            .foregroundColor(.black)
+                                            .font(.title2)
+                                            .bold()
+                                    }
+                                    .background(
+                                        Capsule(style: .continuous)
+                                        .fill(.gray)
+                                        .frame(width: 250, height: 50)
+                                    )
+                                    Spacer()
+                                }
+
+                                // TODO: Have color change based on ready up status...
+
+                            }//: VSTACK
+                        }//: SECTION
+        }
     }
 }
 
