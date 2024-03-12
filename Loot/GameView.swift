284018b84d7b4b6e1655131fa3178a22ececc077
Josh <joshua.singontiko080@myci.csuci.edu>
2024-03-12 10:03:06 -0700

    Fixed player rotations when changing player size

--- Loot/GameView.swift
+++ Loot/GameView.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
 struct GameView: View {
-    let numberOfPlayers: Int = 4
+    let numberOfPlayers: Int = 2
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 ForEach(0..<numberOfPlayers) { playerIndex in
-                    PlayerView(playerIndex: playerIndex, position: playerPosition(playerIndex: playerIndex, totalPlayers: numberOfPlayers, geometry: geometry))
+                    PlayerView(playerIndex: playerIndex, playerCount: numberOfPlayers, position: playerPosition(playerIndex: playerIndex, totalPlayers: numberOfPlayers, geometry: geometry))
                 }
                 CenterCardView(position: CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2))
             }
@@ -24,10 +24,10 @@ struct GameView: View {
             return playerIndex == 0 ? CGPoint(x: center.x, y: height - 50) : CGPoint(x: playerIndex == 1 ? 50 : width - 50, y: center.y)
         case 4:
             switch playerIndex {
-            case 0: return CGPoint(x: center.x, y: height - 50)
-            case 1: return CGPoint(x: 50, y: center.y)
-            case 2: return CGPoint(x: center.x, y: 50)
-            default: return CGPoint(x: width - 50, y: center.y)
+            case 0: return CGPoint(x: center.x, y: height - 50) // Bottom
+            case 1: return CGPoint(x: 50, y: center.y) // Left
+            case 2: return CGPoint(x: center.x, y: 50) // Top
+            default: return CGPoint(x: width - 50, y: center.y) // Right
             }
         default:
             return .zero
@@ -38,6 +38,7 @@ struct GameView: View {
 struct PlayerView: View {
     @State private var numberOfCards: Int = 3
     let playerIndex: Int
+    let playerCount: Int
     let position: CGPoint
     var body: some View {
         ZStack {
@@ -54,16 +55,18 @@ struct PlayerView: View {
                 
             } else {
                 CardStackView(cards: 3) // Other players' card stack
-                    .rotationEffect(angleForPlayer(playerIndex: playerIndex))
+                //let result = [2, 3, 4].contains(number) ? number : defaultValue
+                    .rotationEffect(angleForPlayer(playerIndex: playerIndex, playerCount: playerCount))
                     .position(position)
             }
         }
     }
-    func angleForPlayer(playerIndex: Int) -> Angle {
-        switch playerIndex {
-        case 1: return .degrees(90) // right
-        case 2: return .degrees(180) // top
-        case 3: return .degrees(-90) // left
+    func angleForPlayer(playerIndex: Int, playerCount: Int) -> Angle {
+        print(playerIndex)
+        switch playerCount {
+        case 2: return .degrees(Double(playerIndex * 180))
+        case 3: return .degrees(90 * (pow(2, Double(playerIndex)) - 1))
+        case 4: return .degrees(Double(playerIndex * 90))
         default: return .degrees(0) // player
         }
     }
@@ -89,7 +92,6 @@ struct CardView: View {
 
 struct CardStackView: View {
     var cards: Int
-    
     var body: some View {
         HStack(spacing: -30) {
             ForEach(0..<cards, id: \.self) { index in
