7ad3b3888abea4e4645b4302c096bd114324c95a
Kenna Chase <31677932+Kenna-Chase@users.noreply.github.com>
2024-03-13 13:03:50 -0700

    Implemented color change for ready button

--- Loot/GameLobbyView.swift
+++ Loot/GameLobbyView.swift
@@ -10,30 +10,29 @@ struct GameLobbyView: View {
 
     let peopleTest: [String] = ["Name1", "Name2", "Name3", "Name4"]
 
+    @State var isReady: Bool = false
+
     var body: some View {
         VStack {
 
-        Spacer()
-
-        HStack {
             Spacer()
 
-            Text("Game Room Key: XXX-XXX")
-                    .font(.title)
-                    .foregroundColor(.black)
-                    .bold()
+            HStack {
+                Spacer()
 
-            Spacer()
-       }
-        // Spacer()
+                Text("Game Room Key: XXX-XXX")
+                        .font(.title)
+                        .foregroundColor(.black)
+                        .bold()
+
+                Spacer()
+           }
 
             Section {
                             VStack(alignment: .leading, spacing: 0) {
-                                // ForEach(titles[key]!, id: \.self) { title in
+
                                 ForEach(peopleTest, id: \.self) { player in
-
                                     HStack {
-
                                         Image(systemName: "circle.fill")
                                             .resizable()
                                             .frame(width: 32.0, height: 32.0)
@@ -44,22 +43,23 @@ struct GameLobbyView: View {
                                         Spacer()
                                         Image(systemName: "xmark")
                                             .foregroundColor(Color.red)
-                                        // Setup symbol change "checkmark"
+                                        // TODO: Setup symbol change "checkmark"
                                         Spacer()
                                     }.overlay(
                                         RoundedRectangle(cornerRadius: 10, style: .circular)
                                             .stroke(Color.black, lineWidth: 1)
                                             .frame(width: 350, height: 50)
-                                            // .border(Color.black, width: 3)
-                                    )// HSTACK
+                                    )
                                     .padding(20)
-                                }//: LOOP
+                                }
 
                                 Spacer()
                                 HStack {
                                     Spacer()
                                     Button {
                                         // Ready Up Button Logic
+                                        isReady.toggle()
+                                        // displayViewController.changeView(view: .gameView)
                                     } label: {
                                         Text("Ready")
                                             .foregroundColor(.black)
@@ -68,16 +68,15 @@ struct GameLobbyView: View {
                                     }
                                     .background(
                                         Capsule(style: .continuous)
-                                        .fill(.gray)
+                                        .fill(isReady ? .gray : .green)
                                         .frame(width: 250, height: 50)
                                     )
                                     Spacer()
                                 }
 
-                                // TODO: Have color change based on ready up status...
-
-                            }//: VSTACK
-                        }//: SECTION
+                                Spacer()
+                            }
+            }
         }
     }
 }
