bb2d52627fdb36ae52a1fd9b63715889325a4aa7
Josh <joshua.singontiko080@myci.csuci.edu>
2024-03-23 11:55:44 -0700

    Refactored the GameView to hold a game observable object that holds all the player and card data, also added the center cards which act as fake cards that animate towards each player at the beginning of the round

--- Loot/GameView.swift
+++ Loot/GameView.swift
@@ -1,15 +1,74 @@
 import SwiftUI
 
 struct GameView: View {
-    let numberOfPlayers: Int = 2
+    @EnvironmentObject var viewModel: AppViewModel
+    @State var players: [Player] = [
+        Player(name: "player1", id: UUID()),
+        Player(name: "player2", id: UUID())
+    ]
+    @State var centerCards: [CenterCard] = []
+    @State var playerViews: [PlayerView] = []
+    @StateObject var game: Game
+    init() {
+        let gamePlayers = [
+            GamePlayer(name: "Player 1", cards: []),
+            GamePlayer(name: "Player 2", cards: []),
+            GamePlayer(name: "Player 3", cards: []),
+            GamePlayer(name: "Player 4", cards: [])
+        ]
+        self._game = StateObject(wrappedValue: Game(players: gamePlayers))
+    }
+    let centerPosition = CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                ForEach(0..<numberOfPlayers) { playerIndex in
-                    PlayerView(playerIndex: playerIndex, playerCount: numberOfPlayers, position: playerPosition(playerIndex: playerIndex, totalPlayers: numberOfPlayers, geometry: geometry))
+                ForEach(playerViews.indices, id: \.self) { index in
+                    playerViews[index]
+                    centerCards[index]
+                        .position(centerPosition)
                 }
-                CenterCardView(position: CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2))
             }
+            .onAppear {
+                createPlayerViews(geometry: geometry) // Create PlayerView instances when the view appears
+                createCenterCards()
+            }
+        }
+    }
+    func createCenterCards() {
+        for playerIndex in 0..<game.players.count {
+            let centerCard = CenterCard(
+                number: playerIndex,
+                player: game.players[playerIndex],
+                position: centerPosition,
+                rotation: game.players[playerIndex].rotation
+            )
+            print("creating center card")
+            centerCards.append(centerCard)
+        }
+    }
+    func createPlayerViews(geometry: GeometryProxy) {
+        for playerIndex in 0..<game.players.count {
+            let playerView = PlayerView(
+                player: game.players[playerIndex]
+            )
+            game.players[playerIndex].index = playerIndex
+            game.players[playerIndex].rotation = angleForPlayer(
+                playerIndex: playerIndex,
+                playerCount: game.players.count
+            )
+            game.players[playerIndex].position = playerPosition(
+                playerIndex: playerIndex,
+                totalPlayers: game.players.count,
+                geometry: geometry)
+            playerViews.append(playerView) // Add PlayerView instance to the array
+        }
+    }
+    func angleForPlayer(playerIndex: Int, playerCount: Int) -> Angle {
+        switch playerCount {
+        case 2: return .degrees(Double(playerIndex * 180))
+        case 3: return .degrees(90 * (pow(2, Double(playerIndex)) - 1))
+        case 4: return .degrees(Double(playerIndex * 90))
+        default: return .degrees(0) // player
         }
     }
     func playerPosition(playerIndex: Int, totalPlayers: Int, geometry: GeometryProxy) -> CGPoint {
@@ -21,7 +80,15 @@ struct GameView: View {
         case 2:
             return playerIndex == 0 ? CGPoint(x: center.x, y: height - 50) : CGPoint(x: center.x, y: 50)
         case 3:
-            return playerIndex == 0 ? CGPoint(x: center.x, y: height - 50) : CGPoint(x: playerIndex == 1 ? 50 : width - 50, y: center.y)
+            return playerIndex == 0 ?
+            CGPoint(
+                x: center.x,
+                y: height - 50
+            )
+            : CGPoint(
+                x: playerIndex == 1 ? 50 : width - 50,
+                y: center.y
+            )
         case 4:
             switch playerIndex {
             case 0: return CGPoint(x: center.x, y: height - 50) // Bottom
@@ -33,78 +100,139 @@ struct GameView: View {
             return .zero
         }
     }
-}
-
-struct PlayerView: View {
-    @State private var numberOfCards: Int = 3
-    let playerIndex: Int
-    let playerCount: Int
-    let position: CGPoint
-    var body: some View {
-        ZStack {
-            // Main player's individual card
-            if playerIndex == 0 {
-                let playerCards = CardStackView(cards: numberOfCards) // Main player's card stack
-                HStack(spacing: 10) {
-                    CardView(number: Int.random(in: 1..<8)).onTapGesture {
-                        numberOfCards+=1
-                    } // Main player's card
-                    playerCards
+    struct PlayerView: View {
+        @ObservedObject var player: GamePlayer
+        var body: some View {
+            ZStack {
+                // Main player's individual card
+                if player.index == 0 {
+                    // let playerCards = CardStackView(cards: numberOfCards) // Main player's card stack
+                    // playerHand
+                    VStack {
+                        Text(player.name)
+                        HStack(spacing: 10) {
+                            CardStackView(player: player)
+                            // playerCards
+                        }
+                    }.position(player.position)
+                } else {
+                    VStack {
+                        Text(player.name)
+                        HStack {
+                            CardStackView(player: player)
+                        }
+                        .rotationEffect(player.rotation)
+                    }.position(player.position)
                 }
-                .position(x: position.x, y: position.y + 30)
-
-            } else {
-                CardStackView(cards: 3) // Other players' card stack
-                // let result = [2, 3, 4].contains(number) ? number : defaultValue
-                    .rotationEffect(angleForPlayer(playerIndex: playerIndex, playerCount: playerCount))
-                    .position(position)
             }
         }
     }
-    func angleForPlayer(playerIndex: Int, playerCount: Int) -> Angle {
-        print(playerIndex)
-        switch playerCount {
-        case 2: return .degrees(Double(playerIndex * 180))
-        case 3: return .degrees(90 * (pow(2, Double(playerIndex)) - 1))
-        case 4: return .degrees(Double(playerIndex * 90))
-        default: return .degrees(0) // player
+    struct CardStackView: View {
+        @ObservedObject var player: GamePlayer
+        var body: some View {
+            HStack {
+                ForEach(player.cards.indices, id: \.self) { index in
+                    CardView(card: player.cards[index])
+                }
+            }
         }
+    }
+    struct CenterCard: View {
+        var number: Int
+        let player: GamePlayer
+        @State var position: CGPoint
+        @State var rotation: Angle
+        @State private var isHidden = false // Control visibility
+        var body: some View {
+            if !isHidden { // Only show the view if it's not hidden
+                ZStack(alignment: .topLeading) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.red)
+                        .frame(width: 50, height: 75)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 2))
+                    Text("Loot!")
+                        .font(.system(size: 14))
+                        .foregroundColor(.black)
+                        .padding(4)
+                }
+                .rotationEffect(rotation)
+                .position(position)
+                .onAppear {
+                    withAnimation {
+                        move(to: player.position)
+                    } completion: {
+                        // Add card to player
+                        if player.index == 0 {
+                            player.addCard(Card(number: number, flipped: false))
+                        } else {
+                            player.addCard(Card(number: number, flipped: true))
+                        }
+                        // Hide the view
+                        isHidden = true
+                    }
+                }
+            }
+        }
+        func move(to new: CGPoint) {
+            position = new
+        }
+    }
+
+}
+
+class Game: ObservableObject {
+    @Published var players: [GamePlayer]
+    init(players: [GamePlayer]) {
+        self.players = players
+    }
+    func addCard(toPlayer playerIndex: Int, card: Card) {
+        guard playerIndex >= 0 && playerIndex < players.count else { return }
+        players[playerIndex].addCard(card)
+    }
+}
+
+class GamePlayer: ObservableObject {
+    @Published var cards: [Card] = []
+    let name: String
+    var position: CGPoint = .zero
+    var index: Int = 0
+    var rotation: Angle = .zero
+    init(name: String, cards: [Card]) {
+        self.name = name
+        self.cards = cards
+    }
+    func addCard(_ card: Card) {
+        cards.append(card)
+    }
+}
+
+class Card: ObservableObject {
+    @Published var number: Int
+    @Published var flipped: Bool
+    // Add more properties as needed
+    init(number: Int, flipped: Bool) {
+        self.number = number
+        self.flipped = flipped
     }
 }
 
 struct CardView: View {
     var number: Int
-
+    var flipped: Bool
+    init(card: Card) {
+        self.number = card.number
+        self.flipped = card.flipped
+    }
     var body: some View {
         ZStack(alignment: .topLeading) {
             RoundedRectangle(cornerRadius: 10)
-                .fill(Color.white)
+                .fill(flipped ? Color.red : Color.white) // Change color based on flipped status
                 .frame(width: 50, height: 75)
                 .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 2))
-
-            Text("\(number)")
+            Text(flipped ? "Loot!" : "\(number)")
                 .font(.system(size: 14))
                 .foregroundColor(.black)
                 .padding(4)
         }
-    }
-}
-
-struct CardStackView: View {
-    var cards: Int
-    var body: some View {
-        HStack(spacing: -30) {
-            ForEach(0..<cards, id: \.self) { _ in
-                CardView(number: Int.random(in: 1..<8))
-            }
-        }
-    }
-}
-
-struct CenterCardView: View {
-    let position: CGPoint
-    var body: some View {
-        CardView(number: Int.random(in: 1..<8))
-            .position(position)
     }
 }
