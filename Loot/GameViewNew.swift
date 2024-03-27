e335c021f8f10523597f4e2d7f315b49dda5e29f
Josh <joshua.singontiko080@myci.csuci.edu>
2024-03-26 20:18:51 -0700

    Changed the gameview to stack positions and give cards from the top left corner

---
+++ Loot/GameViewNew.swift
@@ -0,0 +1,18 @@
+//
+//  GameViewNew.swift
+//  Loot
+//
+//  Created by Joshua on 3/26/24.
+//
+
+import SwiftUI
+
+struct GameViewNew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameViewNew()
+}
