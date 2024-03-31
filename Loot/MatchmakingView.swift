edac46e9917d453d2445cf78829bc512809682b4
Kenna Chase <31677932+Kenna-Chase@users.noreply.github.com>
2024-03-30 23:27:37 -0700

    Implemented mockup for matchmaking screen involving displaying existing games

--- Loot/MatchmakingView.swift
+++ Loot/MatchmakingView.swift
@@ -9,29 +9,109 @@ import SwiftUI
 
 struct MatchmakingView: View {
     @EnvironmentObject var viewModel: AppViewModel
+    var testServer1: ServerResponse = ServerResponse(name: "Server 1", key: "0", maximumPlayers: 4, numberOfPlayers: 2, status: "Available")
+    var testServer2: ServerResponse = ServerResponse(name: "Server 2", key: "4", maximumPlayers: 4, numberOfPlayers: 1, status: "In Game")
+    var testServer3: ServerResponse = ServerResponse(name: "Server 3", key: "2", maximumPlayers: 4, numberOfPlayers: 2, status: "Available")
+    var testServer4: ServerResponse = ServerResponse(name: "Server 4", key: "3", maximumPlayers: 4, numberOfPlayers: 4, status: "In Game")
+
+    @State private var createButtonPressed = false
+
     var body: some View {
+        let testServerList: [ServerResponse] = [testServer1, testServer2, testServer3, testServer4]
+
+        let mainBoxWidth = 350
+        let innerBoxWidth = 320
+
         NavigationStack {
             ZStack {
                 Color.lootBeige.ignoresSafeArea(.all)
                 VStack {
-                    List {
-                        ForEach(viewModel.serverList, id: \.key) { server in
-                            Server(server: server)
-                                .onTapGesture {
-                                    // This is where the connect to server function goes
-                                    print("Server \(server.name) tapped, key: \(server.key)")
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color.lootBrown)
+                            .frame(width: CGFloat(mainBoxWidth), height: 500)
+
+                        VStack {
+                            HStack {
+                                Spacer()
+                                Text("Name").font(Font.custom("Quasimodo", size: 16).weight(.medium)).foregroundColor(.lootBeige)
+                                Spacer()
+                                Text("Players").font(Font.custom("Quasimodo", size: 16).weight(.medium)).foregroundColor(.lootBeige)
+                                Spacer()
+                                Text("Status").font(Font.custom("Quasimodo", size: 16).weight(.medium)).foregroundColor(.lootBeige)
+                                Spacer()
+                            }.padding()
+
+                            ScrollView {
+                                Spacer()
+                                ForEach(testServerList, id: \.key) { server in
+
+                                    Server(server: server)
+                                        .onTapGesture {
+                                            // This is where the connect to server function goes
+                                            print("Server \(server.name) tapped, key: \(server.key)")
+                                        }
                                 }
+                            }.frame(width: 350, height: 400)
+
+                            HStack {
+                                Spacer()
+                                Spacer()
+                                Spacer()
+                                Spacer()
+                                Spacer()
+                                Image(systemName: "arrow.clockwise")
+                                    .foregroundColor(.lootBeige)
+                                    .bold()
+                                    .onTapGesture {
+                                    print("Refreshing server list...")
+                                }
+                                Spacer()
+                            }
+
+                        }
+
+                    }.padding()
+
+                    Spacer()
+
+                    // CreateGameButton
+                    ZStack(alignment: .center) {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: CGFloat(mainBoxWidth), height: 50)
+                            .background(Color.lootGreen)
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.black, lineWidth: 2)
+                            )
+                            .shadow(
+                                color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 4, y: 4
+                            )
+                            .scaleEffect(createButtonPressed ? 0.95 : 1.0)
+                            .animation(.easeInOut(duration: 0.1))
+                        Text("Create Game")
+                            .font(Font.custom("Quasimodo", size: 18).weight(.heavy))
+                            .foregroundColor(.black)
+                    }.onTapGesture {
+                        withAnimation {
+                            createButtonPressed.toggle()
                         }
                     }
-                }
-            }
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarBackground()
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Image("dragon")
-                        .resizable()
-                        .scaledToFit()
+                    // Create Game Button End
+
+                    Spacer()
+
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationBarBackground()
+                    .toolbar {
+                        ToolbarItem(placement: .principal) {
+                            Image("dragon")
+                                .resizable()
+                                .scaledToFit()
+                        }
+                    }
                 }
             }
         }
@@ -43,22 +123,35 @@ struct MatchmakingView: View {
 struct Server: View {
     let server: ServerResponse
     var body: some View {
-        HStack {
-            Text(server.name)
-            Spacer()
-            Text("Players: \(server.numberOfPlayers)/\(server.maximumPlayers)")
-            Spacer()
-            Text(server.status)
+
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.lootBeige)
+                .frame(width: 320, height: 50)
+            VStack {
+                HStack {
+                    Spacer()
+                    Text(server.name).font(Font.custom("Quasimodo", size: 14).weight(.medium))
+                    Spacer()
+                    Text(" \(server.numberOfPlayers)/\(server.maximumPlayers)")
+                        .font(Font.custom("Quasimodo", size: 14).weight(.medium))
+                    Spacer()
+                    Image(systemName: "circle.fill").foregroundColor(server.isJoinable() ? .lootGreen : .lootRed)
+                    Spacer()
+                }.padding().containerRelativeFrame(.horizontal).font(Font.custom("Quasimodo", size: 16).weight(.medium))
+
+            }
         }
+
     }
 }
 
-extension View {
+ extension View {
     func navigationBarBackground(_ background: Color = Color.lootBrown) -> some View {
         return self
             .modifier(ColoredNavigationBar(background: background))
     }
-}
+ }
 
 struct ColoredNavigationBar: ViewModifier {
     var background: Color
