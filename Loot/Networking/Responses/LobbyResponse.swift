66db94d686dd076325c9e35886feec7cc7f1a50d
bmichaelr <benjamin.michael774@myci.csuci.edu>
2024-04-03 10:49:57 -0700

    Changed LobbyResponse to expect a name as well

--- Loot/Networking/Responses/LobbyResponse.swift
+++ Loot/Networking/Responses/LobbyResponse.swift
@@ -9,10 +9,12 @@ import Foundation
 
 struct LobbyResponse: Codable {
     var roomKey: String
+    var name: String
     var players: [Player]
     var allReady: Bool
     init() {
         self.roomKey = ""
+        self.name = ""
         self.players = []
         self.allReady = false
     }
