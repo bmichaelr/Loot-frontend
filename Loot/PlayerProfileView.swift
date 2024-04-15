bf7683c92a2369f34921f877f72ab69c04350416
Kenna Chase <31677932+Kenna-Chase@users.noreply.github.com>
2024-04-15 14:49:36 -0700

    Add navigation bar, update profile button, and update formatting to profile view

--- Loot/PlayerProfileView.swift
+++ Loot/PlayerProfileView.swift
@@ -8,94 +8,114 @@
 import SwiftUI
 
 struct PlayerProfileView: View {
-    @State var name: String = "Name"
+    @State var name: String = ""
     private var imageName: String = "loot_"
     @State private var imageNumber: Int = 1
-    @State private var bgColor =
-            Color(.sRGB, red: 0.98, green: 0.9, blue: 0.2)
+    @State private var bgColor = Color.lootBeige
     @State private var profileImagePressed = false
 
     var body: some View {
-        ZStack {
-            Color.lootBeige.ignoresSafeArea(.all)
-            VStack {
-                ZStack {
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .background(Color.lootBrown)
-                    .cornerRadius(10)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(.black, lineWidth: 2)
-                    )
-                    VStack {
+        NavigationStack {
+            ZStack {
+                Color.lootBeige.ignoresSafeArea(.all)
+                VStack {
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .background(Color.lootBrown)
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.black, lineWidth: 2)
+                            )
 
-                        // Profile Image
-                        ZStack {
-                            Circle()
-                                .fill(Color(bgColor))
-                            Image(imageName + String(imageNumber))
-                                .resizable()
-                                .padding()
-                                .aspectRatio(contentMode: .fit)
-                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-
-                        }
-                        .padding()
-                        .frame(width: 200, height: 200, alignment: .center)
-                        .onTapGesture {
-                            profileImagePressed.toggle()
-                        }
-
-                        // Change Profile Image & Background
-                        HStack {
-                            Spacer()
-                            Image(systemName: "arrow.left")
-                                .font(.system(size: 28, weight: .bold))
-                                .foregroundColor(.lootBeige)
-                                .padding()
-                                .onTapGesture {
-                                    // Decrement Image
-                                    if imageNumber - 1 > 0 {
-                                        imageNumber -= 1
-                                    } else {
-                                        imageNumber = 8
-                                    }
-                                }
-
-                            Image(systemName: "arrow.right")
-                                .font(.system(size: 28, weight: .bold))
-                                .foregroundColor(.lootBeige)
-                                .padding()
-                                .onTapGesture {
-                                    // Increment Image
-                                    if imageNumber + 1 <= 8 {
-                                        imageNumber += 1
-                                    } else {
-                                        imageNumber = 0
-                                    }
-                                }
-
-                            ColorPicker("", selection: $bgColor, supportsOpacity: false)
+                        VStack {
+                            Text("Player Profile")
+                                .font(.custom("Quasimodo", size: 30))
+                                .foregroundStyle(Color.lootBeige)
                                 .multilineTextAlignment(.center)
-                                .accentColor(.red)
+                                .padding()
 
-                            Spacer()
+                            ZStack {
+                                Circle()
+                                    .fill(Color(bgColor))
+                                    .stroke(.black, lineWidth: 10)
+                                Image(imageName + String(imageNumber))
+                                    .resizable()
+                                    .padding()
+                                    .aspectRatio(contentMode: .fit)
+                                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+
+                            }
+                            .padding()
+                            .frame(width: 300, height: 300, alignment: .center)
+                            .onTapGesture {
+                                profileImagePressed.toggle()
+                            }
+
+                            HStack {
+                                Spacer()
+                                Image(systemName: "arrow.left")
+                                    .font(.system(size: 28, weight: .bold))
+                                    .foregroundColor(.lootBeige)
+                                    .padding()
+                                    .onTapGesture {
+                                        if imageNumber - 1 > 0 {
+                                            imageNumber -= 1
+                                        } else {
+                                            imageNumber = 8
+                                        }
+                                    }
+
+                                Spacer()
+
+                                ColorPicker("", selection: $bgColor, supportsOpacity: false) .labelsHidden()
+
+                                Spacer()
+
+                                Image(systemName: "arrow.right")
+                                    .font(.system(size: 28, weight: .bold))
+                                    .foregroundColor(.lootBeige)
+                                    .padding()
+                                    .onTapGesture {
+                                        if imageNumber + 1 <= 8 {
+                                            imageNumber += 1
+                                        } else {
+                                            imageNumber = 1
+                                        }
+                                    }
+
+                                Spacer()
+                            }
+
+                            CustomTextField(text: $name, prompt: "Your Name")
+                                .multilineTextAlignment(.center)
+                                .padding()
+
+                            CustomButton(text: "Update Profile") {
+                                print("Create/Modify Profile")
+                            }.padding()
+
                         }
-
-                        // Change Profile Name
-                        CustomTextField(text: $name, prompt: "Your Name").multilineTextAlignment(.center)
 
                     }
+                    .padding([.leading, .trailing], 20)
 
-                }
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationBarBackground()
+                    .toolbar {
+                        ToolbarItem(placement: .principal) {
+                            Image("dragon")
+                                .resizable()
+                                .scaledToFit()
+                        }
+                    }
+                }.padding()
 
-                .padding([.leading, .trailing], 20)
-
-            }.padding()
-        }.sheet(isPresented: $profileImagePressed) {
-            ImageOptionsView(imageNumber: $imageNumber)
-                .presentationDetents([.medium])
+            }.sheet(isPresented: $profileImagePressed) {
+                ImageOptionsView(imageNumber: $imageNumber)
+                    .presentationDetents([.medium])
+            }
         }
     }
 }
